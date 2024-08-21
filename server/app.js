06752238060c1ad4cf800eff69dc8deb2693fa56
Sachin K.C. <sachinxmpl6@gmail.com>
2024-08-21 06:13:28 -0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 const app = express()
-import userRoutes from "./routes/user.js"
 import connectdb from "./utils/connectdb.js";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 
+import userRoutes from "./routes/user.js"
+import chatRoutes from "./routes/chat.js"
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -19,7 +20,7 @@ connectdb(process.env.MONGO_URI)
 //middlewares
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 
 
 
@@ -29,6 +30,7 @@ app.get("/", (req,res)=>{
 
 app.use("/user" , userRoutes)
 app.use("/chat", chatRoutes)
+
 
 
 
