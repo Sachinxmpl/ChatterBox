15b692c4b53827d02b0e097ef6d9ac9acf16a05e
Sachinxmpl <sachinxpml6@gmail.com>
2024-08-22 21:36:22 +0545

    changes made

--- server/test/filex.js
+++ server/test/filex.js
@@ -3,3 +3,84 @@ const b = 20 ;
 console.log(a+b)
 
 
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
+for (let a of ["sachin" , "shamsher" ,"ramesh"]){
+    console.log(a)
+}
